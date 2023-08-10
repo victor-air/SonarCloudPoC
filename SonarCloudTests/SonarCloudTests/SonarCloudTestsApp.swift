4620c23860bff64f9d26f5c2bed1da110dbbebd9
Victor Kreniski <victor.kreniski@airapps.com>
2023-08-10 15:29:45 -0300

    Created Project for testing purposes

---
+++ SonarCloudTests/SonarCloudTests/SonarCloudTestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SonarCloudTestsApp.swift
+//  SonarCloudTests
+//
+//  Created by iOS Developer on 8/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SonarCloudTestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
